4cc9b95cbc1cb666bd7d7f4999c0ca15b20d65b9
Mac <mac@Mac-2.local>
2022-08-04 13:06:25 +0700

    not complete

--- ex02/ft_split.c
+++ ex02/ft_split.c
@@ -6,20 +6,66 @@
 /*   By: mac <sorungta@42bangkok.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 15:36:10 by mac               #+#    #+#             */
-/*   Updated: 2022/08/02 15:44:07 by mac              ###   ########.fr       */
+/*   Updated: 2022/08/04 13:05:54 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_strcpy(char *dest, char *src)
-{
-	int	i;
+#include <stdio.h>
 
-	i = 0;
-	while (src[i] != '\0')
+int	is_in_charset(char c, char *charset)
+{
+	while (*charset)
 	{
-		dest[i] = src[i];
-		i++;
+		if (c == *charset)
+			return (1);
+		++charset;
 	}
-	dest[i] = '\0';
-	return (dest);
+	return (0);
+}
+
+long long	get_word_count(char *str, char *charset)
+{
+	long long	cnt;
+
+	cnt = 0;
+	while (*str)
+	{
+		if (!is_in_charset(*str, charset))
+		{
+			++cnt;
+			while (*str && !is_in_charset(*str, charset))
+				++str;
+		}
+		++str;
+	}
+	return (cnt);
+}
+
+char		**ft_split(char *str, char *charset)
+{
+	char		**ret;
+	long long	idx;
+	char		*from;
+
+	ret = (char**)malloc(sizeof(char*) * get_word_count(str, charset) + 1);
+	idx = 0;
+	while (*str)
+	{
+		if (!is_in_charset(*str, charset))
+		{
+			from = str;
+			while (*str && !is_in_charset(*str, charset))
+				++str;
+			ret[idx] = (char*)malloc(str - from + 1);
+			ft_strcpy(ret[idx++], from, str);
+		}
+		++str;
+	}
+	ret[idx] = 0;
+	return (ret);
+}
+int	main(void)
+{
+	printf("%s\n", ft_split("ixlovexking", "x"));
+	return (0);
 }
